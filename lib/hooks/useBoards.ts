c8ae4d64005f7ebce9e4c58b25c1d30f67d2fbdb
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-11-14 04:58:37 +0900

    feat: 보드 페이지에 작업 생성 기능 추가 및 관련 서비스 구현

--- lib/hooks/useBoards.ts
+++ lib/hooks/useBoards.ts
@@ -1,8 +1,9 @@
 import { useUser } from "@clerk/nextjs";
-import { boardDataService, boardService } from "../services";
+import { boardDataService, boardService, taskService } from "../services";
 import { useEffect, useState } from "react";
 import { Board, Column, ColumnWithTasks } from "../supabase/models";
 import { useSupabase } from "../supabase/SupabaseProvider";
+import { Description } from "@radix-ui/react-dialog";
 
 export function useBoards() {
   const { user } = useUser();
@@ -108,5 +109,41 @@ export function useBoard(boardId: string) {
     }
   }
 
-  return { board, columns, loading, error, updateBoard };
+  async function createRealTask(
+    columnId: string,
+    taskData: {
+      title: string;
+      description?: string;
+      assignee?: string;
+      dueDate?: string;
+      priority: "low" | "medium" | "high";
+    }
+  ) {
+    try {
+      const newTask = await taskService.createTask(supabase!, {
+        title: taskData.title,
+        description: taskData.description || null,
+        assignee: taskData.assignee || null,
+        due_date: taskData.dueDate || null,
+        column_id: columnId,
+        sort_order:
+          columns.find((col) => col.id === columnId)?.tasks.at.length || 0,
+        priority: taskData.priority || "medium",
+      });
+
+      setColumns((prev) =>
+        prev.map((col) =>
+          col.id === columnId ? { ...col, tasks: [...col.tasks, newTask] } : col
+        )
+      );
+
+      return newTask;
+    } catch (error) {
+      setError(
+        error instanceof Error ? error.message : "Failed to create the task"
+      );
+    }
+  }
+
+  return { board, columns, loading, error, updateBoard, createRealTask };
 }
